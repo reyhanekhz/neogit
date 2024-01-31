52b7a0da480d6cde86dd30f14f13f209cca7745e
Reyhane Khayatzade <reyhaneh.kh1384@gmail.com>
2024-02-01 02:36:39 +0330

    global config done

--- main.c
+++ main.c
@@ -8,80 +8,72 @@
 #include <sys/types.h>
 #include <time.h>
 
-int config(const char *key, const char *value)
-{
-    if ( mkdir (".neogit/.config_folder", 0755) != 0)
-    {
-        perror("error creating config directory");
-        return 1;
+#define MAX_ALIASES 128
+
+struct alias_cmd {
+    char *alias;
+    char *command;
+};
+
+struct alias_cmd aliases[MAX_ALIASES];
+int num_aliases = 0;
+
+int num_config = 0;
+
+void add_alias(char *alias, char *command) {
+    strcpy(aliases[num_aliases].alias, alias);
+    strcpy(aliases[num_aliases].command, command);
+    num_aliases++;
+}
+
+
+void exec_alias(char *alias) {
+    for (int i = 0; i < num_aliases; i++) {
+        if (strcmp(aliases[i].alias, alias) == 0) {
+            char cmd[1024];
+            strcpy(cmd, aliases[i].command);
+            system(cmd);
+            break;
+        }
+    }
+}
+
+int globalConfig(const char *key, const char *value) {
+
+    if (access(".neogit/config", F_OK) == -1) {
+        if (mkdir(".neogit/config", 0755) != 0) {
+            perror("error creating config directory");
+            return 1;
+        }
     }
 
-    FILE* file = fopen (".neogit/.config_folder/config", "a");
-    if (file == NULL)
+    if (strcmp(key, "user.name") == 0)
     {
-        perror ("error creating config file");
-        return 1;
-    }
+        FILE* file = fopen(".neogit/config/username", "w");
+        if (file == NULL) {
+            perror("error opening username config file");
+            return 1;
+        }
 
-    fprintf( file, "%s %s\n", key, value);
-    fclose (file);
-    return 0;
+        fprintf(file, "username: %s", value);
+        fclose(file);
+        return 0;
+    }
+    else{
+        FILE* file = fopen(".neogit/config/useremail", "w");
+        if (file == NULL) {
+            perror("error opening useremail config file");
+            return 1;
+        }
+
+        fprintf(file, "useremail: %s", value);
+        fclose(file);
+        return 0;
+    }
 
 }
 
 
-
-//
-//
-//int falias(char alias_code[MAX_COMMAND_LENGTH], char qcommand[MAX_COMMAND_LENGTH])
-//{
-//    char alias[MAX_COMMAND_LENGTH];
-//    char command[MAX_COMMAND_LENGTH];
-//    sscanf(alias_code, "alias.%s", alias);
-//    sscanf(qcommand, "\"%s\"", command);
-//    FILE* file = fopen("alias", "w");
-//    fprintf(file, "%s = %s\n", alias, command);
-//    return 0;
-//}
-//
-//int aliascommand(char alias[MAX_COMMAND_LENGTH])
-//{
-//    FILE *fp;
-//    char aliases[MAX_LINE_LENGTH][MAX_LINE_LENGTH];
-//    char command[MAX_LINE_LENGTH];
-//    char *token;
-//    int i = 0;
-//
-//    fp = fopen("aliases", "r");
-//    if (fp == NULL) {
-//        perror("Error opening aliases file");
-//        return 1;
-//    }
-//
-//    while (fgets(aliases[i], MAX_LINE_LENGTH, fp) != NULL) {
-//        aliases[i][strcspn(aliases[i], "\n")] = '\0';
-//        i++;
-//    }
-//
-//    fclose(fp);
-//
-//    while (1) {
-//        printf("> ");
-//        if (fgets(command, MAX_LINE_LENGTH, stdin) == NULL)
-//            break;
-//
-//        command[strcspn(command, "\n")] = '\0';
-//        for (int j = 0; j < i; j++)
-//        {
-//            token = strtok(aliases[j], "=");
-//            if (strcmp(token, command) == 0) {
-//                system(strtok(NULL, "=\n"));
-//                break;
-//            }
-//        }
-//        return 0;
-//    }
-//}
 
 int main(int argc, char* argv[])
 {
@@ -92,9 +84,15 @@ int main(int argc, char* argv[])
     }
 
     if (strcmp(argv[1], "config") == 0)
-    {
-        return config(argv[3], argv[4]);
-    }
+        return globalConfig(argv[3], argv[4]);
+
+    else if (strcmp(argv[1], "alias") == 0)
+        add_alias(argv[2], argv[3]);
+
+    else
+        exec_alias(argv[1]);
+
+
 
 
     return 0;
